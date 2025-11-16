8a86a916331acb7c217bdad35df4366ebf102db6
CezarySkar <cezary.skar@interia.pl>
2025-11-16 09:29:15 +0100

    Singleton Vault impl

---
+++ WPiAASolution/Singleton/Program.cs
@@ -0,0 +1,6 @@
+﻿using Singleton;
+
+var key1 = Vault.GetInstance().GetVaultKey();
+var key2 = Vault.GetInstance().GetVaultKey();
+
+Console.WriteLine($"Vault\n------\nKey 1: {key1}\nKey 2: {key2}\n------");
---
+++ WPiAASolution/Singleton/Vault.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    internal class Vault
+    {
+        private static Vault? _instance;
+        private static readonly object _lock = new object();
+        private readonly string _vaultKey;
+        private Vault()
+        {
+            _vaultKey = GenerateVaultKey();
+        }
+        public static Vault GetInstance()
+        {
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Vault();
+                    }
+                }
+            }
+            return _instance;
+        }
+
+        public string GetVaultKey()
+        {
+            return _vaultKey;
+        }
+
+        private string GenerateVaultKey()
+        {
+            return Guid.NewGuid().ToString();
+        }
+    }
+}
