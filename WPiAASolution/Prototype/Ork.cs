d32f4bb52038cff449a1c4e1981e6367211eb5ee
CezarySkar <cezary.skar@interia.pl>
2025-12-14 12:20:21 +0100

    Prototype impl

---
+++ WPiAASolution/Prototype/Ork.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    internal class Ork
+    {
+        public int Strength { get; set; }
+
+        public Ork Clone()
+        {
+            string copy = JsonConvert.SerializeObject(this);
+            Ork newOrk = JsonConvert.DeserializeObject<Ork>(copy)!;
+            newOrk.Strength = new Random().Next(1, 100);
+
+            return newOrk;
+        }
+    }
+}
---
+++ WPiAASolution/Prototype/Program.cs
@@ -0,0 +1,16 @@
+﻿using Prototype;
+
+List<Ork> army = [];
+Ork originalOrk = new Ork() { Strength = 50 };
+
+army.Add(originalOrk);
+for (int i = 0; i < 10; i++)
+{
+    Ork clonedOrk = originalOrk.Clone();
+    army.Add(clonedOrk);
+}
+
+foreach (var ork in army)
+{
+    Console.WriteLine($"Nowy ork dołączył do armii, jego siła to: {ork.Strength}");
+}
