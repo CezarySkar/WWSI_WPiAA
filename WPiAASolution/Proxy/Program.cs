a13cf3be8ffc29bc006743741045b5cb5bbb8de9
CezarySkar <cezary.skar@interia.pl>
2025-12-14 15:11:42 +0100

    Proxy impl

---
+++ WPiAASolution/Proxy/Program.cs
@@ -0,0 +1,6 @@
+﻿using Proxy;
+
+Console.WriteLine("Wpisz typ skarbca (open/protected):");
+string vaultType = Console.ReadLine() ?? string.Empty;
+var vaultProxy = new VaultProxy();
+Console.WriteLine(vaultProxy.GetObject(vaultType));
---
+++ WPiAASolution/Proxy/VaultProxy.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    internal class VaultProxy : IVault
+    {
+        private string _vaultPassword = "password";
+        private bool _isAuthenticated = false;
+        public string GetObject(string objTypeToGet)
+        {
+            if (objTypeToGet == "protected")
+            {
+                Console.WriteLine("Podaj hasło");
+                var pass = Console.ReadLine() ?? string.Empty;
+                Authenticate(pass);
+                if (_isAuthenticated)
+                {
+                    IVault vault = new ProtectedVault();
+                    return vault.GetObject("Protected");
+                }
+            }
+            else if (objTypeToGet == "open")
+            {
+                IVault vault = new OpenVault();
+                return vault.GetObject("Open");
+            }
+            return "Access denied.";
+        }
+
+        public bool Authenticate(string password)
+        {
+            if (password == _vaultPassword)
+            {
+                _isAuthenticated = true;
+            }
+            return _isAuthenticated;
+        }
+    }
+}
